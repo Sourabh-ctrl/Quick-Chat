5365f74cd991ce352342bea8a64e66c3bc7b8aeb
Sourabh Lathi <lathisaurav@gmail.com>
2026-07-10 10:29:28 +0530

    optimization

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -6,17 +6,27 @@ import { ChatContext } from "../../context/ChatContext";
 import assets from "../assets/assets";
 
 const Sidebar = () => {
-const [input, setInput] = useState(false);
+  const [input, setInput] = useState(false);
 
-const {logout  , onlineUser } = useContext(AuthContext);
-const {getUsers  , users , selectedUser , setSelectedUser  , unseenMessages , setUnseenMessages} = useContext(ChatContext);
+  const { logout, onlineUser } = useContext(AuthContext);
+  const {
+    getUsers,
+    users,
+    selectedUser,
+    setSelectedUser,
+    unseenMessages,
+    setUnseenMessages,
+  } = useContext(ChatContext);
 
-const filteredUsers = input ? users.filter((user) => user.fullName.toLowerCase().includes(input.toLowerCase())) : users;
+  const filteredUsers = input
+    ? users.filter((user) =>
+        user.fullName.toLowerCase().includes(input.toLowerCase()),
+      )
+    : users;
 
-useEffect(() => {
-  getUsers();
-}, [onlineUser]);
-
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
 
   const navigate = useNavigate();
   return (
@@ -42,10 +52,7 @@ useEffect(() => {
                 Edit Profile
               </p>
               <hr className="my-2 border-t border-gray-500" />
-              <p
-                className="cursor-pointer text-sm"
-                onClick={() => logout()}
-              >
+              <p className="cursor-pointer text-sm" onClick={() => logout()}>
                 Log Out
               </p>
             </div>
@@ -54,7 +61,7 @@ useEffect(() => {
         <div className="bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5">
           <img src={assets.search_icon} alt="search" className="w-3" />
           <input
-          onChange ={(e)=> setInput(e.target.value)}
+            onChange={(e) => setInput(e.target.value)}
             type="text"
             className="bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1"
             placeholder="search user"
@@ -62,22 +69,37 @@ useEffect(() => {
         </div>
       </div>
       <div className="flex flex-col">
-        {
-          filteredUsers.map((user , index) => (
-            <div className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm  ${
-              selectedUser?._id === user._id && 'bg-[#282142]/50' 
-            }`} onClick={() => {setSelectedUser(user) ; setUnseenMessages((prev) => ({...prev, [user._id]: 0})) }} key={index}>
-              <img className="w-[35px] aspect-[1/1] rounded-full" src={user?.profilePic || assets.avatar_icon} alt="avatar" />
-              <div className=""> 
-                <p>{user.fullName}</p>
-                {
-                  onlineUser.includes(user._id) ? <span className="text-green-400 text-xs">Online</span> : <span className="text-neutral-400 text-xs">Offline</span>
-                }
-              </div>
-              {unseenMessages[user._id] > 0 && <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">{unseenMessages[user._id]}</p>}
+        {filteredUsers.map((user) => (
+          <div
+            className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm  ${
+              selectedUser?._id === user._id && "bg-[#282142]/50"
+            }`}
+            onClick={() => {
+              setSelectedUser(user);
+              setUnseenMessages((prev) => ({ ...prev, [user._id]: 0 }));
+            }}
+            key={user._id}
+          >
+            <img
+              className="w-[35px] aspect-[1/1] rounded-full"
+              src={user?.profilePic || assets.avatar_icon}
+              alt="avatar"
+            />
+            <div className="">
+              <p>{user.fullName}</p>
+              {onlineUser.includes(user._id) ? (
+                <span className="text-green-400 text-xs">Online</span>
+              ) : (
+                <span className="text-neutral-400 text-xs">Offline</span>
+              )}
             </div>
-          ))
-        }
+            {unseenMessages[user._id] > 0 && (
+              <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
+                {unseenMessages[user._id]}
+              </p>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
