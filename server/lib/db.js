9ebb79541b979346b97dfd55ddd6fe63a9ad7cd2
Sourabh Lathi <lathisaurav@gmail.com>
2026-07-30 11:01:40 +0530

    formatting

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
- export const connectDB = async () => {
+export const connectDB = async () => {
   try {
-    mongoose.connection.on('connected' , () => console.log("MongoDB connected successfully"));
-     await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`)
+    mongoose.connection.on("connected", () =>
+      console.log("MongoDB connected successfully"),
+    );
+    await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
   } catch (error) {
     console.error("MongoDB connection failed:", error);
   }
-}
-
-
+};
